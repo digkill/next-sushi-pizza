6e8ce9abef721b811cd7db8d948a88d78a7f85ff
Digkill <digkill@mail.ru>
2024-09-04 00:31:52 +0500

    Added header

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -3,6 +3,7 @@ import {cn} from "@/lib/utils";
 import {Container} from "@/components/shared/container";
 import Image from 'next/image'
 import {Button} from '@/components/ui'
+import {ArrowRight, ShoppingCart, User} from "lucide-react";
 
 interface Props {
     className?: string
@@ -23,7 +24,25 @@ export const Header: React.FC<Props> = ({ className }) => {
 
                 {/* Right part */}
                 <div className="flex items-center gap-3">
-                    <Button variant="outline">Войти</Button>
+                    <Button variant="outline" className="flex items-center gap-1">
+                        <User size={16} />
+                        Войти
+                    </Button>
+
+                    <div>
+                        <Button className="group relative">
+                            <b>723 ₽</b>
+                            <span className="h-full w-[1px] bg-white/30 mx-3" />
+                            <div className="flex items-center gap-1 transaction duration-300 group-hover:opacity-0">
+                                <ShoppingCart className="h-4 w-4 relative" strokeWidth={2} />
+                                <b>3</b>
+                            </div>
+                            <ArrowRight
+                                size={20}
+                                className="absolute right-5 transition duration-300 -translate-x-2 opacity-0 group-hover:opacity-100 group-hover:translate-x-0"
+                            />
+                        </Button>
+                    </div>
                 </div>
             </Container>
         </header>
